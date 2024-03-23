499ab1bae731f6708c966e97f65728f15a620e14
Emil4154515 <emil4154515@gmail.com>
2024-03-23 18:05:55 +0400

    update parse files

--- core/schema.py
+++ core/schema.py
@@ -1,7 +1,15 @@
+import datetime
+import re
+
+from django.db.models import DateField, DateTimeField
 from pydantic import ConfigDict, BaseModel
 from pydantic import model_validator, root_validator
+from django.db.models.fields.files import FieldFile, ImageField
+from django.conf import settings
 
 config = ConfigDict(from_attributes=True)
+
+media_path = settings.MEDIA_SRC
 
 
 class BaseSchema(BaseModel):
@@ -10,13 +18,15 @@ class BaseSchema(BaseModel):
     @model_validator(mode='before')
     @classmethod
     def fields_t(cls, instance):
+        if isinstance(instance, dict):
+            return instance
         data = {}
         for name, info in cls.model_fields.items():
             try:
                 field_type = info.annotation.__args__[0]
             except AttributeError:
                 field_type = info.annotation
-
+            # Relations
             if issubclass(field_type, BaseModel):
                 if name in instance._state.fields_cache:
                     data[name] = instance._state.fields_cache[name]
@@ -25,6 +35,19 @@ class BaseSchema(BaseModel):
                     data[name] = values
                 else:
                     data[name] = None
+            # Files
+            elif isinstance(getattr(instance, name), (FieldFile, ImageField)):
+                data[name] = getattr(instance, name)
+                if not getattr(instance, name):
+                    return None
+                value = getattr(instance, name).url
+                data[name] = re.sub(f'.*{media_path}', media_path, value)
+            # Dates
+            # elif isinstance(getattr(instance, name), datetime.datetime):
+            #     data[name] = getattr(instance, name).strftime("%m/%d/%Y, %H:%M:%S")
+            # elif isinstance(getattr(instance, name), datetime.date):
+            #     data[name] = getattr(instance, name).strftime("%m/%d/%Y")
+            # Other
             else:
                 data[name] = getattr(instance, name)
         return data
